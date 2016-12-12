a9993818a9795b566e499b4ed02b8bb69f1caf81
Chenyi Zhang <czhang42@stevens.edu>
2016-12-12 08:37:32 -0500

    added escape_maze

--- amazon_OA/escape_from_maze.py
+++ amazon_OA/escape_from_maze.py
@@ -1,13 +1,55 @@
-map = [[1, 0, 1, 1, 1, 1, 0, 1, 1, 1 ],
-       [1, 0, 1, 0, 1, 1, 1, 0, 1, 1 ],
-       [1, 1, 1, 0, 1, 1, 0, 1, 0, 1 ],
-       [0, 0, 0, 0, 1, 0, 0, 0, 0, 1 ],
-       [1, 1, 1, 0, 1, 1, 1, 0, 1, 0 ],
-       [1, 0, 1, 1, 1, 1, 0, 1, 0, 0 ],
-       [1, 0, 0, 0, 0, 0, 0, 0, 0, 1 ],
-       [1, 0, 1, 1, 1, 1, 0, 1, 1, 1 ],
-       [1, 1, 0, 0, 0, 0, 1, 0, 0, 1 ]]
+map = [[0, 1, 0, 0, 0, 0, 1, 0, 0, 0], 
+       [0, 1, 0, 1, 0, 0, 0, 1, 0, 0], 
+       [0, 0, 0, 1, 0, 0, 1, 0, 1, 0], 
+       [1, 1, 1, 1, 0, 1, 1, 1, 1, 0], 
+       [0, 0, 0, 1, 0, 0, 0, 1, 0, 1], 
+       [0, 1, 0, 0, 0, 0, 1, 0, 1, 1], 
+       [0, 1, 1, 1, 1, 1, 1, 1, 1, 0], 
+       [0, 1, 0, 0, 0, 0, 1, 0, 0, 0], 
+       [0, 0, 1, 1, 1, 1, 0, 1, 1, 0]]
 
-def escape_maze(map):
+def escape_maze(map, dist_x, dist_y):
+    # if we fail at first step... lol
+    if len(map)== 0 or len(map[0]) == 0 or map[0][0] == 1:
+        return 0
 
+    # since map[0][0] is access able at this point
     queue = []
+    step_count = 0
+    # node define (value, y_axis, x_axis)
+    visited = set()  
+    queue.append((0,0))
+    
+    while len(queue) > 0:
+        cur = queue[0]
+        queue.pop(0)
+        if cur in visited:
+            continue
+        visited.add(cur)
+        
+        # check if success
+        # print cur
+        if cur[0] == dist_x and cur[1] == dist_y:
+            return step_count
+
+        if cur[0] >= len(map) or cur[1] >= len(map[0]):
+            continue
+
+        # up
+        if cur[0] - 1 >= 0 and map[cur[0] - 1][cur[1]] == 0:
+            queue.append((cur[0] - 1, cur[1]) )
+        # left
+        if cur[1] - 1 >= 0 and map[cur[0]][cur[1] - 1] == 0:
+            queue.append((cur[0], cur[1] + 1))
+        # down
+        if cur[0] + 1 < len(map) and map[cur[0] + 1][cur[1]] == 0:
+            queue.append((cur[0] + 1, cur[1]))
+        # right
+        if cur[1] + 1 < len(map[0]) and map[cur[0]][cur[1] + 1] == 0:
+            queue.append((cur[0], cur[1] + 1 ))
+
+        step_count += 1
+    # can not reach
+    return -1
+
+print escape_maze(map, 0,0)
