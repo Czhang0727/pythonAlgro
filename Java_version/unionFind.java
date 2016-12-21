5a0dc53a6f5fd09f5e2244b092df9f7201b2c008
pengfei Zhang <andysim3d@gmail.com>
2016-12-20 22:58:35 -0500

    Add Java version template

---
+++ Java_version/unionFind.java
@@ -0,0 +1,22 @@
+import java.lang.*;
+
+class unionFind{
+    private int[] father;
+    public unionFind(Object [] arr){
+        this.father = new int[arr.length];
+    }
+    public int find(int val){
+        if(father[val] != 0){
+            father[val] = find(father[val]);
+        }
+        return father[val];
+    }
+    public void union(int a, int b){
+        int root_a = find(a);
+        int root_b = find(b);
+        if (root_a != root_b){
+            father[root_a] = root_b;
+        }
+    }
+
+}
